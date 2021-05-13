04a4a4c88ac07da9033a7a5d832096c746825d6c
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-05-13 16:50:32 -0500

    setup junit and ran first tests

---
+++ src/main/java/tdd_exercise/App.java
@@ -0,0 +1,17 @@
+package tdd_exercise;
+
+public class App{
+    public static void main(String[] args) {
+        
+    }
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+    public static boolean isEmpty(String str){
+        return str.equals("");
+    }
+}
+
---
+++ src/test/java/tdd_exercise/AppTest.java
@@ -0,0 +1,24 @@
+package tdd_exercise;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    @Test
+    public void testAdd(){
+        assertEquals(App.add(2, 3), 5);
+    }
+    @Test
+    public void testSubtract(){
+        assertEquals(App.subtract(12, 7), 5);
+    }
+    @Test
+    public void isEmpty(){
+        assertTrue(App.isEmpty(""));
+        assertFalse(App.isEmpty("abc"));
+    }
+} 
